01910f954a5962d54baacd542883a53726933869
Vincent Velociter <vincent.velociter@gmail.com>
2023-01-26 17:31:52 +0100

    Init dart package

---
+++ example/result_extensions_example.dart
@@ -0,0 +1,6 @@
+import 'package:result_extensions/result_extensions.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/result_extensions.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library result_extensions;
+
+export 'src/result_extensions_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
